84bd4fa25973d05db8e5a23806aac9beb2a3b73f
Akhtar <hassan.akhtar@sky.uk>
2019-02-28 16:30:47 +0000

    Added EventDashboard

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-
+import { Button } from 'semantic-ui-react'
+import EventDashBoard from '../../features/event/EVentDashboard/EventDashboard'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Welcome to Re-vents</h1>
+        <h1>Re-vents</h1>
+        <EventDashBoard/>
       </div>
     );
   }
