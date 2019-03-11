a73425759d2dcc91c2738abbfab3293c6e02ed66
Akhtar <hassan.akhtar@sky.uk>
2019-03-11 15:55:11 +0000

    end of section 4

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react'
-import EventDashBoard from '../../features/event/EVentDashboard/EventDashboard'
+import React, { Component } from "react";
+import { Container } from "semantic-ui-react";
+import EventDashBoard from "../../features/event/EVentDashboard/EventDashboard";
+import NavBar from "../../features/nav/NavBar/Navbar";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Re-vents</h1>
-        <EventDashBoard/>
+      <div>
+        <NavBar />
+        <Container className="main">
+          <EventDashBoard />
+        </Container>
       </div>
     );
   }
