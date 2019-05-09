00739b0a5dcef0d32c4137c591c8e70730fb94a8
Akhtar <hassan.akhtar@sky.uk>
2019-05-09 14:43:46 +0100

    end of section 6

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
 import { Container } from "semantic-ui-react";
 import { Route } from "react-router-dom";
+import HomePage from "../../features/home/HomePage";
 import EventDashBoard from "../../features/event/EVentDashboard/EventDashboard";
 import NavBar from "../../features/nav/NavBar/Navbar";
+import UserDetailedPage from "../../features/user/UserDetailed/UserDetailedPage";
+import EventForm from "../../features/event/EventForm/EventForm";
+import SettingsDashboard from "../../features/user/Settings/SettingsDashboard";
+import PeopleDashboard from "../../features/user/PeopleDashboard/PeopleDashboard";
+import EventDetailedPage from "../../features/event/EventDetailed/EventDetailedPage";
 
 class App extends Component {
   render() {
@@ -10,7 +16,13 @@ class App extends Component {
       <div>
         <NavBar />
         <Container className="main">
+          <Route exact path="/" component={HomePage} />
           <Route path="/events" component={EventDashBoard} />
+          <Route path="/events/:id" component={EventDetailedPage} />
+          <Route path="/people" component={PeopleDashboard} />
+          <Route path="/profile/:id" component={UserDetailedPage} />
+          <Route path="/settings" component={SettingsDashboard} />
+          <Route path="/createEvent" component={EventForm} />
         </Container>
       </div>
     );
