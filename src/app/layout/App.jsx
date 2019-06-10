1adfc9e4fc805627ee5152fe90ac602fd14aa2ad
Akhtar <hassan.akhtar@sky.uk>
2019-06-10 15:51:04 +0100

    lesson-58-completed

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Container } from "semantic-ui-react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "../../features/home/HomePage";
 import EventDashBoard from "../../features/event/EVentDashboard/EventDashboard";
 import NavBar from "../../features/nav/NavBar/Navbar";
@@ -9,21 +9,35 @@ import EventForm from "../../features/event/EventForm/EventForm";
 import SettingsDashboard from "../../features/user/Settings/SettingsDashboard";
 import PeopleDashboard from "../../features/user/PeopleDashboard/PeopleDashboard";
 import EventDetailedPage from "../../features/event/EventDetailed/EventDetailedPage";
+import TestComponent from "../../features/testarea/TestComponent";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <NavBar />
-        <Container className="main">
+        <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route path="/events" component={EventDashBoard} />
-          <Route path="/events/:id" component={EventDetailedPage} />
-          <Route path="/people" component={PeopleDashboard} />
-          <Route path="/profile/:id" component={UserDetailedPage} />
-          <Route path="/settings" component={SettingsDashboard} />
-          <Route path="/createEvent" component={EventForm} />
-        </Container>
+        </Switch>
+        <Route
+          path="/(.+)"
+          render={() => (
+            <React.Fragment>
+              <NavBar />
+
+              <Container className="main">
+                <Switch>
+                  <Route path="/events" component={EventDashBoard} />
+                  <Route path="/test" component={TestComponent} />
+                  <Route path="/events/:id" component={EventDetailedPage} />
+                  <Route path="/people" component={PeopleDashboard} />
+                  <Route path="/profile/:id" component={UserDetailedPage} />
+                  <Route path="/settings" component={SettingsDashboard} />
+                  <Route path="/createEvent" component={EventForm} />
+                </Switch>
+              </Container>
+            </React.Fragment>
+          )}
+        />
       </div>
     );
   }
