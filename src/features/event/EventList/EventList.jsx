7fb4cf83646c82a16536e3c18aa36db7a03f1c76
Hassan Akhtar ZBk <akhtar.hassan@hotmail.co.uk>
2019-07-07 19:05:44 +0100

    End of section 8

--- src/features/event/EventList/EventList.jsx
+++ src/features/event/EventList/EventList.jsx
@@ -3,15 +3,13 @@ import EventListItem from "./EventListItem";
 
 class EventList extends Component {
   render() {
-    const { events, onEventOpen, deleteEvent } = this.props;
+    const { events, deleteEvent } = this.props;
     return (
       <div>
-        <h1>Event List</h1>
         {events.map(events => (
           <EventListItem
             key={events.id}
             event={events}
-            onEventOpen={onEventOpen}
             deleteEvent={deleteEvent}
           />
         ))}
